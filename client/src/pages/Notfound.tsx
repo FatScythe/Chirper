c4371052bbb5fc26be6e59e744e7deebf9ac7fae
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-12-31 21:36:02 +0100

    Added a not found page

---
+++ client/src/pages/Notfound.tsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+  return (
+    <section className='flex justify-center items-center h-screen'>
+      <div className='text-center'>
+        <h2 className='text-bold text-4xl text-red-300'>404</h2>
+        <p className='text-6xl font-semibold'>
+          Page not found Err <span>: )</span>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
