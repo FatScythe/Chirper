1f79fbfcb9c12a524f61ad600fd93d27bfd9853f
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-05 21:32:13 +0100

    Made some needed changes

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,22 +1,30 @@
 import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useOutletContext } from "react-router-dom";
 // Context
 // import { useAuth } from "../context/AuthContext";
 // Components
 import AllChats from "../component/chat/AllChats";
 
+type ContextType = {
+  setIsChatsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
 const Chat = () => {
   const [isChatsOpen, setIsChatsOpen] = useState(true);
+  const navigate = useNavigate();
+
   return (
     <section className='grid grid-cols-12 h-screen relative sm:static bg-dark/90 text-white overflow-hidden'>
       <aside
-        className={`p-4 sm:p-2 bg-dark sm:col-span-4 fixed sm:static z-10 ${
+        className={`p-2 bg-dark sm:col-span-4 fixed sm:static z-10 ${
           isChatsOpen ? "left-0 right-0" : "-left-full"
         } top-0 bottom-0 transition-all duration-200`}
       >
         <h1
           className='text-xl sm:text-2xl md:text-3xl font-semibold'
-          onClick={() => setIsChatsOpen(false)}
+          onClick={() => {
+            navigate("/chats");
+          }}
         >
           Chats
         </h1>
@@ -27,16 +35,16 @@ const Chat = () => {
             className='bg-transparent border-transparent border border-b-primary w-full outline-none focus:border-2'
           />
         </div>
-        <AllChats />
+        <AllChats isChatsOpen={isChatsOpen} setIsChatsOpen={setIsChatsOpen} />
       </aside>
       <main className='col-span-12 sm:col-span-8'>
-        <h1 className='text-xl font-bold' onClick={() => setIsChatsOpen(true)}>
-          Single Chat
-        </h1>
-        <Outlet />
+        <Outlet context={{ setIsChatsOpen } satisfies ContextType} />
       </main>
     </section>
   );
 };
 
+export function useChatOpen() {
+  return useOutletContext<ContextType>();
+}
 export default Chat;
