c0b39c4ca52ffa145439e1fbdb079e5880b116ad
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-05 21:31:37 +0100

    Added Paper Send Icon

--- client/src/component/icon.tsx
+++ client/src/component/icon.tsx
@@ -1,4 +1,8 @@
-export const LockClosedIcon = ({ ...props }) => {
+type Props = {
+  className: string;
+};
+
+export const LockClosedIcon = ({ className }: Props) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -6,7 +10,7 @@ export const LockClosedIcon = ({ ...props }) => {
       viewBox='0 0 24 24'
       strokeWidth={1.5}
       stroke='currentColor'
-      className={props.className || ""}
+      className={className}
     >
       <path
         strokeLinecap='round'
@@ -17,7 +21,7 @@ export const LockClosedIcon = ({ ...props }) => {
   );
 };
 
-export const LockOpenIcon = ({ ...props }) => {
+export const LockOpenIcon = ({ className }: Props) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -25,12 +29,31 @@ export const LockOpenIcon = ({ ...props }) => {
       viewBox='0 0 24 24'
       strokeWidth={1.5}
       stroke='currentColor'
-      className={props.className || ""}
+      className={className}
     >
       <path
         strokeLinecap='round'
         strokeLinejoin='round'
         d='M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z'
+      />
+    </svg>
+  );
+};
+
+export const SendIcon = ({ className }: Props) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className={className}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5'
       />
     </svg>
   );
