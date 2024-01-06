cf9448dc4d96a9ef8e92f9ea16efa950ed55a205
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-06 22:33:36 +0100

    Feat: User can now send messsages

--- client/src/component/message/MessageInput.tsx
+++ client/src/component/message/MessageInput.tsx
@@ -1,14 +1,49 @@
+import { useState } from "react";
+// Icon
 import { SendIcon } from "../icon";
 
-const MessageInput = () => {
+type Props = {
+  chatId: string;
+};
+
+const MessageInput = ({ chatId }: Props) => {
+  const [text, setText] = useState("");
+  const sendMessage = async () => {
+    try {
+      const response = await fetch("/api/v1/message", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ chatId, text }),
+      });
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert(data?.msg || "Couldn't send message");
+        return;
+      }
+      setText("");
+    } catch (error) {
+      console.error(error);
+      alert("Message was not sent");
+    }
+  };
+
   return (
     <div className='flex justify-between items-center w-full'>
       <textarea
         rows={1}
         className='basis-11/12 p-2 rounded-3xl bg-transparent outline-none border border-primary resize-none'
         placeholder='Message...'
+        value={text}
+        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+          setText(e.target.value);
+        }}
       />
-      <button className='basis-1/12  mx-auto'>
+      <button
+        className='basis-1/12 mx-auto disabled:opacity-30'
+        onClick={() => sendMessage()}
+        disabled={!text}
+      >
         <SendIcon className='w-10 h-10 bg-dark p-2 rounded-full' />
       </button>
     </div>
