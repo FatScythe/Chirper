0fb23b79a15a6c16c3bf702b5a167f2e67ee5749
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-07 08:55:52 +0100

    Feat: Made major improvements by reducing fetch calls

--- client/src/component/message/ChatMessages.tsx
+++ client/src/component/message/ChatMessages.tsx
@@ -1,62 +1,24 @@
-import { useState, useEffect } from "react";
 // Types
-import { IMessage } from "../../model/message";
+import { IChat } from "../../model/chat";
 // Components
 import MessageBubble from "./MessageBubble";
 
 type Props = {
-  chatId: string;
+  chat: IChat;
 };
 
-const ChatMessages = ({ chatId }: Props) => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [messages, setMessages] = useState<IMessage[] | null>(null);
-
-  const getChatMessages = async (chatId: string) => {
-    try {
-      const response = await fetch("/api/v1/message/" + chatId);
-      const data = await response.json();
-
-      if (!response.ok) {
-        setLoading(false);
-        setError(true);
-        alert(data?.msg || "Couldn't get chat messages");
-        return;
-      }
-
-      setLoading(false);
-      setMessages(data);
-    } catch (err) {
-      setLoading(false);
-      setError(true);
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    getChatMessages(chatId);
-  }, [chatId]);
-
-  if (loading) {
-    return <div>Loading...</div>;
+const ChatMessages = ({ chat }: Props) => {
+  if (chat.messages === undefined || !chat.messages) {
+    return <></>;
   }
 
-  if (error) {
-    return <div>Error...</div>;
-  }
-
-  if (messages && messages.length > 0) {
-    return (
-      <div className='h-full w-full'>
-        {messages.map((message) => {
-          return <MessageBubble key={message.id} message={message} />;
-        })}
-      </div>
-    );
-  }
-
-  return <></>;
+  return (
+    <div className='h-full w-full'>
+      {chat.messages.map((message) => {
+        return <MessageBubble key={message.id} message={message} />;
+      })}
+    </div>
+  );
 };
 
 export default ChatMessages;
