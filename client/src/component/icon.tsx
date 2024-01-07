5e2c4c74ef2b8d8836c71c3be1d02d5527cfe09b
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-07 08:30:50 +0100

    Added Option and chevron back icon

--- client/src/component/icon.tsx
+++ client/src/component/icon.tsx
@@ -58,3 +58,41 @@ export const SendIcon = ({ className }: Props) => {
     </svg>
   );
 };
+
+export const OptionIcon = ({ className }: Props) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className={className}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z'
+      />
+    </svg>
+  );
+};
+
+export const ChevronLeftIcon = ({ className }: Props) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className={className}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M15.75 19.5 8.25 12l7.5-7.5'
+      />
+    </svg>
+  );
+};
