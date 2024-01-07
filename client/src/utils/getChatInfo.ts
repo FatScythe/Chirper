c029cd29ce8ef99413b3f0cc779dccd9e4176e84
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-07 08:30:04 +0100

    Created fn to get a chats info and a url variable

---
+++ client/src/utils/getChatInfo.ts
@@ -0,0 +1,58 @@
+// Types
+import { IChat } from "../model/chat";
+import { IUser } from "../model/user";
+// Image
+import groupAvatar from "../assets/avatars.png";
+
+export const getChatInfo = (chat: IChat, user: IUser) => {
+  /*
+    This function gets a chat and a user and returns a name, the chat last message, members and image for the chat
+    It check if the chat is a private / group chat. 
+    It also checks If it's a private chat,it also checks if it belong to the current user.
+   */
+  if (!user) {
+    return { name: chat.name, image: "", lastmessage: "", members: [] };
+  }
+
+  let lastmessage = "";
+  if (chat.messages && chat.messages.length > 0) {
+    lastmessage = chat.messages[chat.messages.length - 1].text;
+  }
+
+  let members: IUser[] = chat.users;
+
+  if (chat.chatType === "private") {
+    const isMyChat =
+      chat.createdBy === user.userId && chat.members.length === 1;
+    if (isMyChat) {
+      const currentUser = members.filter(
+        (chatUser) => chatUser.id === user.userId
+      )[0];
+
+      return {
+        name: chat?.name || "Me(You)",
+        image: currentUser.avatar,
+        lastmessage,
+        members,
+      };
+    } else {
+      const chatPartner = chat.users.filter(
+        (chatUser) => chatUser.id !== user.userId
+      )[0];
+
+      return {
+        name: chatPartner.name,
+        image: chatPartner.avatar,
+        lastmessage,
+        members,
+      };
+    }
+  } else {
+    return {
+      name: chat?.name || "Group " + chat.id, // Add a default group image
+      image: groupAvatar,
+      lastmessage,
+      members,
+    };
+  }
+};
