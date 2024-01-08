65f3d06800c01729b18de179ba192e5d14ca1b70
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-08 22:06:59 +0100

    Feat: Added Chat Info and Option UI for single chat

--- client/src/component/chat/ChatHeader.tsx
+++ client/src/component/chat/ChatHeader.tsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
 // Icons
 import { ChevronLeftIcon, OptionIcon } from "../icon";
 // Context
 import { useAuth } from "../../context/AuthContext";
 // Type
 import { IChat } from "../../model/chat";
-// Util
+// Utils
 import { getChatInfo } from "../../utils/getChatInfo";
 import url from "../../utils/url";
 
@@ -15,6 +16,7 @@ type Props = {
 
 const ChatHeader = ({ chat, setIsChatsOpen }: Props) => {
   const { user } = useAuth();
+  const [showOption, setShowOption] = useState(false);
 
   if (!user) {
     return <></>;
@@ -38,12 +40,55 @@ const ChatHeader = ({ chat, setIsChatsOpen }: Props) => {
 
         <div>
           <h2 className='font-semibold'>{name}</h2>
-          <small>{members.map((member) => member.name + ", ")} ...</small>
+          <small>
+            {chat.chatType === "group"
+              ? members.map((member) => member.name + "") + "..."
+              : "last seen " +
+                new Date(chat.updatedAt ? chat.updatedAt : "").toDateString()}
+          </small>
         </div>
       </div>
 
-      <div>
-        <OptionIcon className='w-6 h-6' />
+      <div className='relative'>
+        <button onClick={() => setShowOption(!showOption)}>
+          <OptionIcon className='w-6 h-6' />
+        </button>
+        {showOption && <Option chat={chat} setShowOption={setShowOption} />}
+      </div>
+    </div>
+  );
+};
+
+type optionType = {
+  chat: IChat;
+  setShowOption: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Option = ({ chat, setShowOption }: optionType) => {
+  const { user } = useAuth();
+
+  return (
+    <div className=''>
+      <div
+        className='fixed z-20 bg-transparent top-0 right-0 left-0 bottom-0'
+        onClick={() => setShowOption(false)}
+      ></div>
+      <div className='absolute z-20 top-6 -left-28 bg-white h-fit w-28 text-black'>
+        <ul className='text-center flex flex-col justify-stretch gap-2 items-center'>
+          <li className='w-full py-1 hover:bg-danger hover:text-white cursor-pointer duration-200'>
+            {chat.createdBy === user?.userId ? "Delete Chat" : "Leave Chat"}
+          </li>
+          {chat.chatType === "group" && (
+            <>
+              <li className='w-full py-1 hover:bg-dark hover:text-white cursor-pointer duration-200'>
+                Add to group
+              </li>
+              <li className='w-full py-1 hover:bg-danger hover:text-white cursor-pointer duration-200'>
+                Leave group
+              </li>
+            </>
+          )}
+        </ul>
       </div>
     </div>
   );
