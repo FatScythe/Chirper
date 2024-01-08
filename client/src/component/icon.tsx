c5589acee47cdba70434164c1df0c3b44872715a
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-08 22:08:47 +0100

    Added Plus Icon

--- client/src/component/icon.tsx
+++ client/src/component/icon.tsx
@@ -115,3 +115,22 @@ export const LogoutIcon = ({ className }: Props) => {
     </svg>
   );
 };
+
+export const AddIcon = ({ className }: Props) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className={className}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M12 4.5v15m7.5-7.5h-15'
+      />
+    </svg>
+  );
+};
