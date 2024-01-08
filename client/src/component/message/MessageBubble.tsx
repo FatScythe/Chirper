cd66d7d45ea42732c25b6493f119c0fd3bbefa47
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-08 22:10:19 +0100

    Refactored and added option button to message

--- client/src/component/message/MessageBubble.tsx
+++ client/src/component/message/MessageBubble.tsx
@@ -3,29 +3,42 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 // Types
 import { IMessage } from "../../model/message";
+import { OptionIcon } from "../icon";
 
 type Props = {
   message: IMessage;
+  setShowOption: React.Dispatch<React.SetStateAction<boolean>>;
 };
-const MessageBubble = ({ message }: Props) => {
+const MessageBubble = ({ message, setShowOption }: Props) => {
   const { user } = useAuth();
 
   if (!user) {
     return <Navigate to='/' />;
   }
 
-  const isCurrentUser = user.userId === message.sender;
+  const isUserMessage = user.userId === message.sender;
 
-  if (!isCurrentUser) {
-    return (
-      <div className='my-2 flex justify-start items-center'>
-        <p className='bg-gray-700 mx-3 p-2 rounded-md'>{message.text}</p>
-      </div>
-    );
-  }
   return (
-    <div className='my-2 flex justify-end items-center'>
-      <p className='bg-primary mx-3 p-2 rounded-md'>{message.text}</p>
+    <div
+      className={`my-2 flex ${
+        isUserMessage ? "justify-end group" : "justify-start"
+      } items-center cursor-pointer gap-2 `}
+    >
+      <p
+        className={`${
+          isUserMessage ? "bg-primary mr-3 order-2" : "bg-gray-700 ml-3 order-1"
+        } p-2 rounded-md`}
+      >
+        {message.text}
+      </p>
+      <button
+        onClick={() => setShowOption(true)}
+        className={`${
+          isUserMessage ? "order-1" : "order-2"
+        } hidden group-hover:block hover:bg-white/45 rounded-full p-1`}
+      >
+        <OptionIcon className='w-6 h-6 rotate-90' />
+      </button>
     </div>
   );
 };
