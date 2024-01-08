0471dd4e7b079cc7e9cac41761e6393e2b8f0695
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-08 22:11:11 +0100

    Feat: Added Logout fnality, Refactored Code

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { Outlet, useNavigate, useOutletContext } from "react-router-dom";
 // Context
-// import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 // Components
 import AllChats from "../component/chat/AllChats";
+// Icon
+import { LogoutIcon } from "../component/icon";
+import CreateChat from "../component/chat/CreateChat";
 
 type ContextType = {
   setIsChatsOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -12,6 +15,7 @@ type ContextType = {
 const Chat = () => {
   const [isChatsOpen, setIsChatsOpen] = useState(true);
   const navigate = useNavigate();
+  const { logout } = useAuth();
 
   return (
     <section className='grid grid-cols-12 h-screen relative sm:static bg-dark/90 text-white overflow-hidden'>
@@ -20,20 +24,24 @@ const Chat = () => {
           isChatsOpen ? "left-0 right-0" : "-left-full"
         } top-0 bottom-0 transition-all duration-200`}
       >
-        <h1
-          className='text-xl sm:text-2xl md:text-3xl font-semibold'
-          onClick={() => {
-            navigate("/chats");
-          }}
-        >
-          Chats
-        </h1>
-        <div className='my-4'>
-          <input
-            type='search'
-            placeholder='Search or start a new chat'
-            className='bg-transparent border-transparent border border-b-primary w-full outline-none focus:border-2'
-          />
+        <div className='flex justify-between items-center px-2'>
+          <h1
+            className='text-xl sm:text-2xl md:text-3xl font-semibold'
+            onClick={() => navigate("/chats")}
+          >
+            Chats
+          </h1>
+
+          <button
+            className='p-2 rounded-full hover:bg-danger'
+            onClick={() => logout()}
+            title='logout'
+          >
+            <LogoutIcon className='w-6 h-6' />
+          </button>
+        </div>
+        <div className='my-4 flex justify-start items-center gap-2 relative'>
+          <CreateChat />
         </div>
         <AllChats isChatsOpen={isChatsOpen} setIsChatsOpen={setIsChatsOpen} />
       </aside>
