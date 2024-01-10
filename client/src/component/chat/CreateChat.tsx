274519be043076f63a6bba167c2737dd771a19c2
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-10 21:53:10 +0100

    Styling: Made adjustment to user

--- client/src/component/chat/CreateChat.tsx
+++ client/src/component/chat/CreateChat.tsx
@@ -5,11 +5,11 @@ import { AddIcon } from "../icon";
 const CreateChat = () => {
   const [showModal, setShowModal] = useState(false);
   return (
-    <>
+    <div className='flex justify-start items-center gap-2 w-full relative'>
       <input
         type='search'
         placeholder='Search chats'
-        className='bg-transparent border-transparent border border-b-primary w-full outline-none focus:border-2'
+        className='bg-transparent border-transparent border border-b-primary w-full outline-none focus:border-2 basis-11/12'
       />
       {showModal && <Modal setShowModal={setShowModal} />}
       <button
@@ -19,7 +19,7 @@ const CreateChat = () => {
       >
         <AddIcon className='w-6 h-6' />
       </button>
-    </>
+    </div>
   );
 };
 
@@ -29,12 +29,20 @@ type ModalType = {
 
 const Modal = ({ setShowModal }: ModalType) => {
   return (
-    <div
-      className='fixed bg-secondary/35 top-0 left-0 right-0 bottom-0'
-      onClick={() => setShowModal(false)}
-    >
-      <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-red-300 rounded-md w-11/12 sm:w-1/2 h-40'>
-        Add Chat modal
+    <div className='relative'>
+      <div
+        className='fixed h-full w-full bg-transparent top-0 left-0 right-0 bottom-0'
+        onClick={() => setShowModal(false)}
+      ></div>
+      <div className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-black rounded-md w-11/12 sm:w-1/2 h-1/3 p-2'>
+        <div>
+          <h2>Chat type</h2>
+        </div>
+        <input
+          type='search'
+          placeholder='Search chats'
+          className='bg-transparent border-transparent border border-b-primary w-full outline-none focus:border-2 basis-11/12'
+        />
       </div>
     </div>
   );
