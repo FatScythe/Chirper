7722b185e3353fee91cffc0e21624522ba469505
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-14 11:02:39 +0100

    Implementing socketio

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ require("express-async-errors");
 require("dotenv").config();
 const express = require("express");
 const { sequelize } = require("./model");
+const { createServer } = require("node:http");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+// const cors = require("cors");
+const { Server, socket } = require("socket.io");
 // Middlewares
 const errorMiddleware = require("./middleware/error-handler");
 const notFoundMiddleware = require("./middleware/not-found");
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
+// app.use(cors());
 app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
@@ -28,7 +39,15 @@ app.use(errorMiddleware);
 app.use(notFoundMiddleware);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, async () => {
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(PORT, async () => {
   try {
     console.log("App is listening on port: " + PORT);
     await sequelize.authenticate();
