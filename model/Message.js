70f85ba981a52d7da0e2f1ced430634b3a175032
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-20 07:50:11 +0100

    Fix: Changed message text field to type text

--- model/Message.js
+++ model/Message.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Message = sequelize.define("message", {
     text: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
     },
     sender: {
