26cfd413be96ecdfc0e714c1b82869dcb27b9f3d
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-21 14:18:31 +0100

    Added safe guard incase all chat is not open on mobile screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { useAuth } from "./context/AuthContext";
 import { useSocket } from "./context/SocketContext";
 // Router
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Link } from "react-router-dom";
 // Pages
 import Chat from "./pages/Chat";
 import Register from "./pages/Register";
@@ -32,8 +32,18 @@ function App() {
           <Route
             index
             element={
-              <div className='grid place-items-center text-3xl italic font-semibold h-screen'>
-                Pick or create a Chat
+              <div className='flex flex-col justify-center items-center gap-5 font-semibold h-screen'>
+                <h2 className='text-2xl sm:text-3xl italic'>
+                  Pick or create a Chat
+                </h2>
+                <div className='w-full text-center sm:hidden'>
+                  <Link
+                    to='/'
+                    className='sm:hidden bg-primary px-4 py-2 rounded-2xl'
+                  >
+                    Go back to chats
+                  </Link>
+                </div>
               </div>
             }
           />
