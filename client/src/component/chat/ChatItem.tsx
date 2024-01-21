5d1ea41cd5695c1f239eee674a1cf9cd5c9019d0
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-01-21 18:25:44 +0100

    Feat: Implemented Unread message

--- client/src/component/chat/ChatItem.tsx
+++ client/src/component/chat/ChatItem.tsx
@@ -27,10 +27,47 @@ const ChatItem = (props: Props) => {
     return <Navigate to='/' />;
   }
 
+  const getUnreadMessage = (chat: IChat): number => {
+    /*
+      // DANGER - To tired to think for a better solution and i don't want to make
+      multiple patch calls to the server when a message is read
+      This fn takes the current chat and gets it copy in the local storage
+      returns the difference btw the live chat and local storage chat 
+    */
+    if (localStorage.getItem("chats")) {
+      let myLocalChats: IChat[] = JSON.parse(
+        localStorage.getItem("chats") || "[]"
+      );
+      myLocalChats = myLocalChats.filter(
+        (singleChat) => chat.id === singleChat.id
+      );
+
+      let chatMessages = myLocalChats[0]?.messages;
+
+      if (chatMessages && chatMessages.length > 0 && chat.messages) {
+        let messageDiff = chat.messages.length - chatMessages.length;
+
+        // If the lastmessage belongs to the current user then, they couldn't have
+        // unread
+        if (
+          chat.messages.length > 0 &&
+          chat.messages[chat.messages.length - 1].sender === user.userId
+        ) {
+          return 0;
+        }
+
+        return Math.abs(messageDiff);
+      }
+    }
+    return 0;
+  };
+
+  let unreadMsg = getUnreadMessage(chat);
+
   return (
     <NavLink
       to={"/chats/" + chat.id}
-      className={`chat-item flex justify-start items-center gap-4 my-2 sm:p-2 rounded-md hover:bg-white/5`}
+      className={`chat-item flex justify-start items-center gap-4 my-2 py-3 px-1 rounded-md hover:bg-white/5`}
       onClick={() => setIsChatsOpen(!isChatsOpen)}
     >
       <img
@@ -42,8 +79,17 @@ const ChatItem = (props: Props) => {
         className='h-8 w-8 rounded-full'
       />
       <main className='w-4/5 text-ellipsis overflow-hidden'>
-        <h3 className='whitespace-nowrap text-sm font-bold w-4/5 overflow-hidden text-ellipsis'>
-          {getChatInfo(chat, user).name}
+        <h3 className=' overflow-hidden flex justify-between items-baseline'>
+          <span className='whitespace-nowrap text-sm font-bold w-4/5 text-ellipsis'>
+            {getChatInfo(chat, user).name}
+          </span>
+          <div className='flex justify-center items-'>
+            {unreadMsg > 0 && (
+              <small className='bg-success rounded-full p-2 w-4 h-4 text-[0.6rem] flex justify-center items-center'>
+                {unreadMsg}
+              </small>
+            )}
+          </div>
         </h3>
         {currentChat?.id === chat.id && memberTyping ? (
           <small className='text-success font-semibold'>{memberTyping}</small>
